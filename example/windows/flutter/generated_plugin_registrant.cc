7dbc8b6d52e4bc4877449b5787a18418cbb75410
Aitaro Chaya <aitaro.chaya@gmail.com>
2022-02-27 11:32:00 +0900

    chore: update dependences

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -2,6 +2,13 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
-void RegisterPlugins(flutter::PluginRegistry* registry) {}
+#include <url_launcher_windows/url_launcher_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  UrlLauncherPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+}
